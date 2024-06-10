32020baab1eea2278793b57115c7694326c26ed0
rokzc <chirag.garg2020a@vitstudent.ac.in>
2024-06-10 15:22:40 +0530

    Basics

--- reactt/src/App.js
+++ reactt/src/App.js
@@ -1,10 +1,26 @@
-
-import Ram from './newfile'
+import { useState } from "react";
 function App() {
+  
+  let [counter,setCounter]=useState(1);
+  function addvalue(){
+    if(counter<20){
+    counter=counter+1;
+    setCounter(counter)}
+  }
+  function decreaseValue () {
+    if(counter>0){
+    counter=counter-1;
+    setCounter(counter)
+    }
+
+  }
   return (
-    <>
-    <h1>Jai shree Ram</h1>
-    <Ram></Ram>
+   <>
+   <h1>Count value {counter}</h1>
+   <button onClick={addvalue}>Increment</button>
+   <br>
+   </br>
+   <button onClick={decreaseValue}>Decrement</button>
     </>
   );
 }
